cacdd7ca42013ea815723bb1f1cc5a91a491160c
owenbadgley <143543330+owenbadgley@users.noreply.github.com>
2024-06-18 15:03:53 -0400

    state 7

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import Message from "./Message";
 import { useState } from "react";
 
 function App() {
-  const [drink, setDrink] = useState({
-    title: "Americano",
-    price: 5,
+  const [customer, setCustomer] = useState({
+    name: "John",
+    address: {
+      city: "San Francisco",
+      zipCode: 94111,
+    },
   });
 
   const handleClick = () => {
-    setDrink({ ...drink, price: 6 });
+    setCustomer({
+      ...customer,
+      address: { ...customer.address, zipCode: 94112 },
+    });
   };
 
   return (
