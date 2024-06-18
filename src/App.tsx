1efd6a65d5e65c7feb61e70644ad20a7fd83e21b
owenbadgley <143543330+owenbadgley@users.noreply.github.com>
2024-06-18 13:28:56 -0400

    set up files for css module

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import ListGroup from "./components/ListGroup";
-import { useState } from "react";
-import { AiFillBug } from "react-icons/ai";
+import Button from "./components/Button/Button";
 
 function App() {
-  let items = ["New York", "Los Angeles", "San Francisco"];
-  const heading = "Cities";
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <AiFillBug></AiFillBug>
-      <ListGroup
-        items={items}
-        heading={heading}
-        onSelectItem={handleSelectItem}
-      ></ListGroup>
+      <Button onClick={() => {}}>My Button</Button>
     </div>
   );
 }
