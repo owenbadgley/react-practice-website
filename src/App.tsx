7cf595f3e89686ad4642d2a011b6d28b74370480
owenbadgley <143543330+owenbadgley@users.noreply.github.com>
2024-07-01 11:59:36 -0400

    excercise 3

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,34 @@ import NavBar from "./components/NavBar";
 import Cart from "./components/Cart";
 
 function App() {
-  const [game, setGame] = useState({ id: 1, player: { name: "John" } });
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
+  });
 
   const handleClick = () => {
-    setGame({ ...game, player: { ...game.player, name: "OBOE" } });
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: (item.quantity += 1) } : item
+      ),
+    });
   };
+
   return (
     <div>
-      <p>{game.player.name}</p>
-      <button onClick={handleClick}>Update Name</button>
+      <p>Cart</p>
+      <ul>
+        {cart.items.map((item) => (
+          <li key={item.id}>
+            {item.title} - [{item.quantity}]
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
