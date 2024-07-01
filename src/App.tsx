21c401efea9ff9312bd576faa135a158dc9850ca
owenbadgley <143543330+owenbadgley@users.noreply.github.com>
2024-07-01 11:20:19 -0400

    stage 10

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
 import Message from "./Message";
 import { useState } from "react";
+import produce from "immer";
 
 function App() {
   const [bugs, setBugs] = useState([
     { id: 1, title: "bug 1", fixed: false },
-    { id: 2, title: "bug 2", fixed: "false" },
+    { id: 2, title: "bug 2", fixed: false },
   ]);
 
   const handleClick = () => {
-    setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) bug.fixed = true;
+      })
+    );
+
+    //setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
   };
 
   return (
     <div>
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title} {bug.fixed ? "fixed" : "new"}
+        </p>
+      ))}
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
