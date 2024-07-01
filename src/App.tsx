d3620a8aecb43d67a66b6c5d534b0e2cd8d7eab4
owenbadgley <143543330+owenbadgley@users.noreply.github.com>
2024-07-01 12:43:58 -0400

    Building Forms 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
+import Form from "./components/form";
 
 function App() {
-  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
   return (
     <div>
-      <NavBar cartItemsCount={cartItems.length}></NavBar>
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])}></Cart>
+      <Form></Form>
     </div>
   );
 }
