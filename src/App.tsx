0e519108ab9a9538b5f4ce3b1f79fadadc8e1d08
owenbadgley <143543330+owenbadgley@users.noreply.github.com>
2024-07-01 11:15:39 -0400

    state 9

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,13 @@ import Message from "./Message";
 import { useState } from "react";
 
 function App() {
-  const [tags, setTags] = useState(["happy", "cheerful"]);
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "bug 1", fixed: false },
+    { id: 2, title: "bug 2", fixed: "false" },
+  ]);
 
   const handleClick = () => {
-    //Add
-    setTags([...tags, "exciting"]);
-
-    //Remove
-    setTags(tags.filter((tag) => tag !== "happy"));
-
-    //Update
-    setTags(tags.map((tag) => (tag === "happy" ? "happiness" : tag)));
+    setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
   };
 
   return (
