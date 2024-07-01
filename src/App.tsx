0f01b245ec920f138c0df21f84423ef9b216dd61
owenbadgley <143543330+owenbadgley@users.noreply.github.com>
2024-07-01 11:40:19 -0400

    excercise 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import NavBar from "./components/NavBar";
 import Cart from "./components/Cart";
 
 function App() {
-  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
+  const [game, setGame] = useState({ id: 1, player: { name: "John" } });
+
+  const handleClick = () => {
+    setGame({ ...game, player: { ...game.player, name: "OBOE" } });
+  };
   return (
     <div>
-      <NavBar cartItemsCount={cartItems.length}></NavBar>
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])}></Cart>
+      <p>{game.player.name}</p>
+      <button onClick={handleClick}>Update Name</button>
     </div>
   );
 }
