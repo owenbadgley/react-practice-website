55593d74d5fd4825dd749b90089e3bc8aeae301b
owenbadgley <143543330+owenbadgley@users.noreply.github.com>
2024-07-03 11:56:35 -0400

    backend 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import { useEffect, useRef, useState } from "react";
 import ProductList from "./components/ProductList";
 
-function App() {
-  const [category, setCategory] = useState("");
+const connect = () => console.log("connecting");
+const disconnect = () => console.log("disconnecting");
 
-  return (
-    <div>
-      <select
-        name=""
-        id=""
-        className="form-select"
-        onChange={(event) => setCategory(event.target.value)}
-      >
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      {<ProductList category={category}></ProductList>}
-    </div>
-  );
+function App() {
+  useEffect(() => {
+    connect();
+    return () => disconnect();
+  });
+
+  return <div></div>;
 }
 
 export default App;
