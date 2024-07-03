54e23214031d737374ec807c24777e06e7cc4d7b
owenbadgley <143543330+owenbadgley@users.noreply.github.com>
2024-07-03 12:05:48 -0400

    backend 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import { useEffect, useRef, useState } from "react";
-import ProductList from "./components/ProductList";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const connect = () => console.log("connecting");
-const disconnect = () => console.log("disconnecting");
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
   useEffect(() => {
-    connect();
-    return () => disconnect();
-  });
-
-  return <div></div>;
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data));
+  }, []);
+  return (
+    <div>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
