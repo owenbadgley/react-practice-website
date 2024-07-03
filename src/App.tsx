9085889749a910fcafbec8433f84bc7dffc93896
owenbadgley <143543330+owenbadgley@users.noreply.github.com>
2024-07-03 12:46:33 -0400

    backend 12

--- src/App.tsx
+++ src/App.tsx
@@ -44,10 +44,26 @@ function App() {
       });
   };
 
+  const addUser = () => {
+    const newUser = { id: 0, name: "Owen" };
+    const originalUsers = [...users];
+    setUsers([...users, newUser]);
+
+    axios
+      .post("https://jsonplaceholder.typicode.com/users/", newUser)
+      .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
   return (
     <div>
       {error && <p className="text-danger">{error}</p>}
       {isLoading && <div className="spinner-border"></div>}
+      <button className="btn btn-primary mb-3" onClick={addUser}>
+        Add
+      </button>
       <ul className="list-group">
         {users.map((user) => (
           <li
